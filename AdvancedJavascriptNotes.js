9acf0ec7ab9734b763ab28267467ddcf99221502
Yael Brown <yaelrbrown@gmail.com>
2019-01-10 03:13:32 -0500

    Update AdvancedJavascriptNotes.js
    Added notes about scope, IIFE functions and 'this' keyword.

--- AdvancedJavascriptNotes.js
+++ AdvancedJavascriptNotes.js
@@ -24,7 +24,8 @@ function moo(arg) {
   delete arg;
 };
 
-// cannot use eval keyword. Eval allows use you execute arbitrary lines of code. 
+// cannot use eval keyword. Eval allows use you execute arbitrary lines of code.
+// What goes on in eval() stays in eval(). Does not leak out to other code.
 
 
 
@@ -136,6 +137,8 @@ Function (or local) Scope or Global scope.
 Variable hoisting is when js splits a variable (when use strict is used and a variable has not been defined yet).
   Declares the variable at the top of the document (hoists it to the top)
   then redefines it. 
+  Automatic hoisting of the variable declaration of the enclosing scope
+
   Ex: 
     "use strict";
     console.log(d);
@@ -147,10 +150,66 @@ Variable hoisting is when js splits a variable (when use strict is used and a va
     console.log(d);
     d = 1;
 
+Scope chain is defined, lexically. In order in which it is defined on the page. 
+Looks inside it's inner scope, then outer scope, til it finds it's variable. 
+The scope is defined by how the code is physically written.
+The variable is resolved in the order in which the code is written on the page. 
 
-
-
-
+IIFE - Imediately Invoked Function Expression. - When an anonymous function is wrapped and then immediately invoked. 
+Does not allow any pseudo global variable to leak out into global scope.
 */
 
+(function() {
+  var e = {'hello': 'main'};
+  console.log("main: ", e);
+})();     // The empty brackets immediate call the function. 
+
+/*
+
+? Function closures are inner functions that have access to the outer (enclosing) functions variables-scope chain. 
+*/
+
+// Interview question that tests function closures. 
+var foo = [];
+for (var i = 0; i < 10; i++) {
+  foo[i] = function() { return i };
+}
+
+console.log(foo[0]());    // 10
+console.log(foo[1]());    // 10
+console.log(foo[2]());    // 10
+
+/* 
+To change previous function to print a different number everytime
+  primitive types are passed by value not by reference.
+*/
+
+var foo = [];
+for (var i = 0; i < 10; i++) {
+  (function() {
+    var y = i;
+    foo[i] = function() { return y };
+  })();     // IIFE
+}
+
+console.log(foo[0]());    // 0
+console.log(foo[1]());    // 1
+console.log(foo[2]());    // 2
+
+
+
+/* 
+? Using 'this' outside of a function, refers to the global scope
+This is determined the calling of the context. It was determined by the way the function is called. 
+
+It has different values depending on where it is used:
+
+In a method, this refers to the owner object.
+Alone, this refers to the global object.
+In a function, this refers to the global object.
+In a function, in strict mode, this is undefined.
+In an event, this refers to the element that received the event.
+Methods like call(), and apply() can refer this to any object.
+
+*/
 
